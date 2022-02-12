28df34a4b837cd2ca93a892e734f606a99b54ee6
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-12 23:21:58 +0530

    ADDED: Checking for option ladder snake noplay

--- Program.cs
+++ Program.cs
@@ -6,12 +6,40 @@ namespace SnakeAndLadder
     {
         static void Main(string[] args)
         {
+            //Initializing Constant
+            const  int LADDER = 1;
+            const int NOPLAY = 2;
+            const int SNAKE = 3;
+
             int position = 0;
 
             Random random = new Random();
 
             int noOnDie = random.Next(1, 7);
             Console.WriteLine($"No On Die {noOnDie}");
+
+            int options = random.Next(1, 4);
+
+            switch (options)
+            {
+                case LADDER:
+                    position += noOnDie;
+                    break;
+
+                case SNAKE:
+                    position -= noOnDie;
+                    break;
+
+                case NOPLAY:
+                    Console.WriteLine("POSITION " + position);
+                    break;
+
+
+            }
+
+            Console.WriteLine("POSITION " + position);
+
+
         }
     }
 }
